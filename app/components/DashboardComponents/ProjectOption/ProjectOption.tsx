7882f106e09c38398d987f232ca43ce3992e0e51
Lythmass <lythmass17@gmail.com>
2024-03-30 17:28:16 +0400

    style: design projects

--- app/components/DashboardComponents/ProjectOption/ProjectOption.tsx
+++ app/components/DashboardComponents/ProjectOption/ProjectOption.tsx
@@ -1,8 +1,11 @@
+import { raleway } from 'fonts';
 import { ProjectType } from 'types';
 
 export const ProjectOption: React.FC<ProjectType> = (props) => {
   return (
-    <div className='w-full flex gap-1'>
+    <div
+      className={`${raleway.className} font-medium w-full flex gap-1 hover:bg-dark hover:text-light cursor-default px-2 py-1`}
+    >
       <h1>{props.id}.</h1>
       <h1>{props.project}</h1>
     </div>
