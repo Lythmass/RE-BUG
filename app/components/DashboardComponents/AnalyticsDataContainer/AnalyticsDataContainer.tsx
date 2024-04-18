3be0a10c594d28b3b04489e44006a08e4c153a99
Lythmass <lythmass17@gmail.com>
2024-04-18 20:04:11 +0400

    feat: add analytics

--- app/components/DashboardComponents/AnalyticsDataContainer/AnalyticsDataContainer.tsx
+++ app/components/DashboardComponents/AnalyticsDataContainer/AnalyticsDataContainer.tsx
@@ -1,7 +1,42 @@
+'use client';
+import { FetchAuthDataActions } from 'actions';
 import { ContainerLayout } from 'components';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+import { ProjectType } from 'types';
 
 export const AnalyticsDataContainer = () => {
-  return <ContainerLayout width='75%'>AnalyticsDataContainer</ContainerLayout>;
+  const [project, setProject] = useState<ProjectType>();
+  const params = useParams();
+  useEffect(() => {
+    const fetchProject = async () => {
+      const response = await FetchAuthDataActions(`/projects/get/${params.id}`);
+      if (response.status == 404) {
+        alert(response.data.name);
+      }
+      setProject(response.data);
+      return response;
+    };
+    fetchProject();
+  }, [params.id]);
+  return (
+    <ContainerLayout width='75%'>
+      <h1>Project: {project?.project}</h1>
+      <h1>Total Bugs: {project?.bugs?.length}</h1>
+      <h1>
+        Oldest Bug:{' '}
+        {project?.bugs &&
+          new Date(project?.bugs[0]?.created_at).toLocaleDateString()}
+      </h1>
+      <h1>
+        Newest Bug:{' '}
+        {project?.bugs &&
+          new Date(
+            project?.bugs[project?.bugs?.length - 1]?.created_at,
+          ).toLocaleDateString()}
+      </h1>
+    </ContainerLayout>
+  );
 };
 
 export default AnalyticsDataContainer;
--- app/types/ProjectType.d.ts
+++ app/types/ProjectType.d.ts
@@ -1,5 +1,8 @@
+import { BugType } from './BugType';
+
 export type ProjectType = {
   id: number;
   project: string;
   project_id: number;
+  bugs?: BugType[];
 };
