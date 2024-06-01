b9bb27487f027833f2a2d8d847fe68deb4c15af0
Lythmass <lythmass17@gmail.com>
2024-06-01 16:27:24 +0400

    fix: type error in before attribute

--- app/components/DashboardComponents/BugStatusAndSeverity/BugStatusAndSeverity.tsx
+++ app/components/DashboardComponents/BugStatusAndSeverity/BugStatusAndSeverity.tsx
@@ -3,10 +3,10 @@ import { BugStatusAndSeverityType } from './BugStatusAndSeverityType';
 export const BugStatusAndSeverity: React.FC<BugStatusAndSeverityType> = (
   props,
 ) => {
-  const before = `before:content-[attr(before)] before:hidden hover:before:block before:absolute before:left-1/2 before:-translate-x-1/2 before:top-10 before:bg-dark before:min-w-[14rem] before:py-4 before:px-3 before:rounded-xl before:z-[200] before:transition-all`;
+  const before = `before:content-[attr(data-before)] before:hidden hover:before:block before:absolute before:left-1/2 before:-translate-x-1/2 before:top-10 before:bg-dark before:min-w-[14rem] before:py-4 before:px-3 before:rounded-xl before:z-[200] before:transition-all`;
   return (
     <h1
-      before={props.description}
+      data-before={props.description}
       style={{ backgroundColor: props.color }}
       className={`${before} px-2 flex text-light items-center rounded-full relative`}
     >
