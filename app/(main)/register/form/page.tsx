648cdd4d027b92640b2e068ec0316a39ac8e65c9
Lythmass <lythmass17@gmail.com>
2024-06-01 16:47:29 +0400

    fix: remove suspense and get searchParams from props

--- app/(main)/register/form/page.tsx
+++ app/(main)/register/form/page.tsx
@@ -3,37 +3,33 @@ import { registerAction } from 'actions';
 import { AuthChoice, AuthRegisterForm, AuthTitle } from 'components';
 import { authException } from 'exceptions';
 import { fillFormData } from 'helpers';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense } from 'react';
+import { useRouter } from 'next/navigation';
 import { useForm } from 'react-hook-form';
 import { AuthTypes } from 'types';
 
-export default function RegisterAdmin() {
+export default function RegisterAdmin({
+  searchParams,
+}: {
+  searchParams: { role_id: number; role: string };
+}) {
   const methods = useForm<AuthTypes>({ mode: 'all' });
-  const searchParams = useSearchParams();
   const router = useRouter();
 
   const handleSubmit = async (data: AuthTypes) => {
     const formData = fillFormData(data);
-    formData.append('role_id', `${searchParams.get('role_id')}`);
+    formData.append('role_id', `${searchParams.role_id}`);
     authException({ router, methods, formData, action: registerAction });
   };
   return (
-    <Suspense>
-      <div className='w-full h-full flex flex-col items-center p-10 gap-10'>
-        <AuthTitle text={`register as ${searchParams.get('role')} on`} />
-        <form
-          onSubmit={methods.handleSubmit(handleSubmit)}
-          className='w-full flex flex-col gap-5'
-        >
-          <AuthRegisterForm methods={methods} />
-        </form>
-        <AuthChoice
-          before='Already have and account?'
-          middle='Log in'
-          href='/'
-        />
-      </div>
-    </Suspense>
+    <div className='w-full h-full flex flex-col items-center p-10 gap-10'>
+      <AuthTitle text={`register as ${searchParams.role} on`} />
+      <form
+        onSubmit={methods.handleSubmit(handleSubmit)}
+        className='w-full flex flex-col gap-5'
+      >
+        <AuthRegisterForm methods={methods} />
+      </form>
+      <AuthChoice before='Already have and account?' middle='Log in' href='/' />
+    </div>
   );
 }
